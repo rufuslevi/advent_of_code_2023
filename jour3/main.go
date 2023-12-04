951c3cce7e9bcf820bafef4c40d578e8c7e25b59
Michael Roussel <michaelroussel404@outlook.com>
2023-12-03 21:23:42 -0500

    jour3 init

---
+++ jour3/main.go
@@ -0,0 +1,13 @@
+package jour3
+
+import "advent_of_code_2023/utils"
+
+func Main() {
+	inputLines := utils.ReadFileLines("jour3/input.txt")
+	utils.LogDayAnswers(inputLines, []func([]string) (string, error){part1})
+}
+
+func part1(inputLines []string) (string, error) {
+
+	return "", nil
+}
